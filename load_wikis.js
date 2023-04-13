4978981fc54185abbd90d64c2fed945d80ba53f2
Alejandro González Benítez <91694249+agonben23@users.noreply.github.com>
2023-04-13 08:02:00 +0000

    errores a medio arreglar

--- load_wikis.js
+++ load_wikis.js
@@ -1,29 +1,20 @@
 function loadWikis(){
 
-  let data;
+let data;
 
-  fetch("./wikis.json")
-  .then(response => {
-   return response.json();
-})
-.then(jsondata => data = jsondata);
-
-console.log(data)
-
-const wikis = JSON.parse(data);
-
-let text = "";
-
-for(let i = 0; i < wikis.length; i++){
-
-  text = text + "<li class=\"wikis\"><a href=" + 
-  concat("\"./wikis/",wikis[i].name)+"\">" + 
-   wikis[i].name + "</a></li>\n<br>\n"
-
-  console.log(wikis[i].name + " " + wikis[i].href)
-   
-}
-
-   document.getElementById("list").innerHTML = text
+fetch("./wikis.json")
+  .then(response => response.json())
+  .then(jsondata => {
+    data = jsondata;
+    const wikis = JSON.parse(data);
+    let text = "";
+    for(let i = 0; i < wikis.length; i++){
+      text = text + "<li class=\"wikis\"><a href=" + 
+        concat("\"./wikis/",wikis[i].name)+"\">" + 
+        wikis[i].name + "</a></li>\n<br>\n"
+      console.log(wikis[i].name + " " + wikis[i].href)
+    }
+    document.getElementById("list").innerHTML = text;
+  });
 
 }
